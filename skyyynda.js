8a19b0bfea06183b08c0a952b195b834201f6c0d
jackyinf <zeka.rum@gmail.com>
2016-10-03 17:39:05 +0300

    correct skyyynda server configuration

--- skyyynda.js
+++ skyyynda.js
@@ -1,4 +1,21 @@
-var express = require('express');
-var app = express();
-app.use('/', express.static(__dirname + '/dist')); // ? adjust
-app.listen(3000, function() { console.log('listening'); });
+// var express = require('express');
+// var app = express();
+// app.use('/', express.static(__dirname + '/dist')); // ? adjust
+// app.listen(3000, function() { console.log('listening'); });
+
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 3000;
+const app = express();
+
+// serve static assets normally
+app.use(express.static(__dirname + '/dist'));
+
+// handle every other route with index.html, which will contain
+// a script tag to your application's JavaScript file(s).
+app.get('*', function (request, response){
+  response.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
+});
+
+app.listen(port);
+console.log("server started on port " + port);
