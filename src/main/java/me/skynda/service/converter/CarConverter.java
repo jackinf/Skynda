a3eec7771d20758b64303f8f4b6d36165bc6f014
Mihhail Marenkov <Mihhail Marenkov>
2016-10-03 22:28:10 +0300

    convertation simplifyer 2

---
+++ src/main/java/me/skynda/service/converter/CarConverter.java
@@ -0,0 +1,12 @@
+package me.skynda.service.converter;
+
+import me.skynda.dto.SingleCarDataDto;
+import me.skynda.model.Car;
+
+public class CarConverter {
+	
+	public SingleCarDataDto transform(Car car) {
+		return null;
+	}
+
+}
