4f6280dc58d3b7f08ba76a48d998fe8878265d3a
Ardi Aver <reva.ardi@gmail.com>
2016-10-15 10:33:34 +0300

    merge conflict resolve

--- src/routes/Details/index.js
+++ src/routes/Details/index.js
@@ -5,13 +5,10 @@ export default (store) => ({
   getComponent(nextState, cb) {
     require.ensure([], (require) => {
       const Details = require("./containers/DetailsContainer").default;
-<<<<<<< HEAD
-      injectReducer(store, { key: "isLoading", reducer: require("./reducers/Details.module.toggle-loading").default });
-      injectReducer(store, { key: "carData", reducer: require("./reducers/Details.module.car-data.js").default });
-=======
+
       injectReducer(store, {key: "isLoading", reducer: require("./reducers/Details.module.toggle-loading").default});
       injectReducer(store, {key: "carData", reducer: require("./reducers/Details.module.car-data.js").default});
->>>>>>> 57f113de15547cac67368c94072cf6508e8943ff
+
       cb(null, Details);
     }, "details");
   }
