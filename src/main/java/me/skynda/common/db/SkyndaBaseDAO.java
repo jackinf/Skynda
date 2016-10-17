c94be36c11eb279a2852bb06c0ed9488ee23195d
jevgenir <jevgeni.rumjantsev@finestmedia.ee>
2016-10-17 10:46:24 +0300

    comments

--- src/main/java/me/skynda/common/db/SkyndaBaseDAO.java
+++ src/main/java/me/skynda/common/db/SkyndaBaseDAO.java
@@ -5,17 +5,28 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
- * Base DAO implementation for SKYNDA backend projects.
+ * Base data access object implementation for SKYNDA backend project.
  */
 public abstract class SkyndaBaseDAO {
 
+    /**
+     * Session creator object.
+     */
 	@Autowired
 	private SessionFactory sessionFactory;
 
+    /**
+     * Gets database session
+     * @return Hibernate's session
+     */
     protected Session getSession() {
         return sessionFactory.getCurrentSession();
     }
 
+    /**
+     * Id generator based on existing database id's.
+     * @return new id.
+     */
     public String getNextUuid() {
         return "" + getSession().createSQLQuery("select nextval('uuid_seq')").uniqueResult();
     }
--- src/main/java/me/skynda/model/Car.java
+++ src/main/java/me/skynda/model/Car.java
@@ -18,11 +18,15 @@ import lombok.EqualsAndHashCode;
 
 import javax.persistence.Table;
 
+/**
+ * Main car model
+ */
 @Entity
 @Data
 @Table(name = "cars_for_sale")
 public class Car {
 
+	// TODO: Add id to base model class
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
@@ -30,32 +34,84 @@ public class Car {
 	@ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "model_code", nullable = false)
     private CarModels carModels;
-	
+
+	/**
+	 *
+	 */
 	@OneToMany(mappedBy = "car", fetch = FetchType.LAZY)
     private List<CarReport> carReport;
-	
+
+	/**
+	 * Reviewer's comments and ratings of this specific car.
+	 */
 	@OneToMany(mappedBy = "car", fetch = FetchType.LAZY)
     private List<CarReview> carReview;
 	
 	private String vinCode;
+
+	/**
+	 * How much does the car costs.
+	 */
 	private BigDecimal price;
+
+	/**
+	 * TODO: Add to base model
+	 */
 	private Date created;
-	//TODO FK customerId 
+
+	/**
+	 * TODO: private Date updated, and add it to base model
+	 */
+
+	//TODO FK customerId
+
+	/**
+	 * Car's registration number for... TODO
+	 */
 	private String registrationNumber;
+
+	/**
+	 * What is the last registered mileage the car has driven.
+	 */
 	private String mileage;
+
+	/**
+	 * Car's paint from the outside (on doors, roof etc).
+	 */
 	private String colorOutside;
+
+	/**
+	 * Car's interior's color.
+	 */
 	private String colorInside;
+
+	/**
+	 * Url's of the car.
+	 */
 	private String images;
+
+	/**
+	 * Is the car sold (not available for sale anymore).
+	 */
 	private Boolean isSold;
 	private String fuelCity;
 	private String fuelHighway;
 	private String features;
+
+	/**
+	 * TODO: array of string.
+	 * List of problems with the car.
+	 */
 	private String problems;
 	private Integer compressionRatio;
 	private String compressionType;
 	private String configuration;
 	private String cylinders;
 	private String displacement;
+
+	/**
+	 * 95, 98, Diesel.
+	 */
 	private String fuelType;
 	private Integer size;
 	private Integer torque;
