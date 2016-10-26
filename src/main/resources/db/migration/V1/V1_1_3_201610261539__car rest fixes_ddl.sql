bb1ee3726c6fde203948f7c7f45ee285f4b264a9
jevgenir <jevgeni.rumjantsev@finestmedia.ee>
2016-10-26 15:53:17 +0300

    migration 1_1_3

---
+++ src/main/resources/db/migration/V1/V1_1_3_201610261539__car rest fixes_ddl.sql
@@ -0,0 +1,40 @@
+DROP TABLE IF EXISTS "car_image" CASCADE
+;
+
+DROP TABLE IF EXISTS "car_feature" CASCADE
+;
+
+DROP TABLE IF EXISTS "car_fault" CASCADE
+;
+
+/* For storing image url-s for car */
+CREATE TABLE "car_image"
+(
+	"id" serial,
+	"image_url" varchar(100),
+	"cars_for_sale_id" integer REFERENCES "cars_for_sale" (id)
+)
+;
+
+/* For storing car features */
+CREATE TABLE "car_feature"
+(
+	"id" serial,
+	"text" varchar(100),
+	"cars_for_sale_id" integer REFERENCES "cars_for_sale" (id)
+)
+;
+
+/* for storing car problems */
+CREATE TABLE "car_fault"
+(
+	"id" serial,
+	"image_url" varchar(100),
+	"text" varchar(100),
+	"cars_for_sale_id" integer REFERENCES "cars_for_sale" (id)
+)
+;
+
+/* They are now foreign keys, no need for those columns */
+ALTER TABLE "cars_for_sale" DROP COLUMN "features";
+ALTER TABLE "cars_for_sale" DROP COLUMN "images";
