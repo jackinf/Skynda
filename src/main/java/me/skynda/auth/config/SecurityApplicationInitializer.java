8b5255eae5635a849ab33175fdf4459327960183
jevgenir <jevgeni.rumjantsev@finestmedia.ee>
2016-10-26 19:42:43 +0300

    recovered security

--- src/main/java/me/skynda/auth/config/SecurityApplicationInitializer.java
+++ src/main/java/me/skynda/auth/config/SecurityApplicationInitializer.java
@@ -1,7 +1,7 @@
 package me.skynda.auth.config;
 
-//import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-//
-//public class SecurityApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-//
-//}
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
