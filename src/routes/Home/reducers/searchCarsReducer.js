93bb2f9b11d81c17385af4ffe909c82fcd6fc37e
Ardi Aver <reva.ardi@gmail.com>
2016-11-01 17:10:59 +0200

    asg[as

--- src/routes/Home/reducers/searchCarsReducer.js
+++ src/routes/Home/reducers/searchCarsReducer.js
@@ -2,11 +2,7 @@ import {setIsSearching} from './../actions/index';
 import React from "react";
 
 const makeSearch = (input) => {
-  console.log("MAKE_SEARCH input", input);
   var searchValues = {...input.buttonGroupValues, ...input.sliderValues};
-  console.log("INPUT brngrpvals", input.buttonGroupValues);
-  console.log("INPUT slidervls", input.sliderValues);
-  console.log("INPUT RETURN", searchValues);
 
   return (dispatch) => {
     dispatch(setIsSearching(true));
