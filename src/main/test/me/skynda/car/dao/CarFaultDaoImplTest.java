312ae44e383d28a5f0ebf273f53304713a3ea0e4
jevgenir <jevgeni.rumjantsev@finestmedia.ee>
2016-11-10 17:23:53 +0200

    flyway sample test

--- src/main/test/me/skynda/car/dao/CarFaultDaoImplTest.java
+++ src/main/test/me/skynda/car/dao/CarFaultDaoImplTest.java
@@ -1,25 +1,42 @@
 package me.skynda.car.dao;
 
-import org.junit.After;
-import org.junit.Before;
+import me.skynda.car.model.Car;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class CarFaultDaoImplTest {
-    @Before
-    public void setUp() throws Exception {
 
-    }
+    CarFaultDaoImpl carFaultDao = new CarFaultDaoImpl();
 
-    @After
-    public void tearDown() throws Exception {
-
-    }
+//    @Before
+//    public void setUp() throws Exception {
+//        // setup the session factory
+//
+//        Configuration configuration = new Configuration();
+//        configuration.addAnnotatedClass(Car.class)
+//                .addAnnotatedClass(CarModel.class)
+//                .addAnnotatedClass(CarManufacturer.class);
+//
+//        configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+//        configuration.setProperty("hibernate.connection.driver_class", "org.h2.Driver");
+//        configuration.setProperty("hibernate.connection.url", "jdbc:h2:mem");
+//        configuration.setProperty("hibernate.hbm2ddl.auto", "create");
+//
+//        sessionFactory = configuration.buildSessionFactory();
+//        session = sessionFactory.openSession();
+//    }
 
     @Test
     public void addMultipleToCar() throws Exception {
-
+        carFaultDao.addMultipleToCar(new Car(), new ArrayList<>());
     }
 
 }
