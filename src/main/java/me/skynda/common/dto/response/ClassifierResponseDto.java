fa82d2f0963b7d060bac20fdde176abaa6f679a8
jevgenir <jevgeni.rumjantsev@finestmedia.ee>
2016-11-13 20:29:54 +0200

    classifiers controller service

---
+++ src/main/java/me/skynda/common/dto/response/ClassifierResponseDto.java
@@ -0,0 +1,10 @@
+package me.skynda.common.dto.response;
+
+import lombok.Data;
+
+@Data
+public class ClassifierResponseDto {
+    private int id;
+    private String value;
+    private String name;
+}
