7fd63e8604a6140ef2c43161b69ee77a1420c12c
jevgenir <jevgeni.rumjantsev@finestmedia.ee>
2016-11-20 17:49:37 +0200

    whooops

--- src/main/java/me/skynda/vehicle/dao/VehicleReportItemDaoImpl.java
+++ src/main/java/me/skynda/vehicle/dao/VehicleReportItemDaoImpl.java
@@ -21,7 +21,7 @@ public class VehicleReportItemDaoImpl extends SkyndaBaseEntityDaoImpl<VehicleRep
 
         Session session = getSession();
         String id = vehicle.getId().toString();
-        session.createSQLQuery("DELETE FROM vehicle_description WHERE vehicle_id = "  + id)   // TODO: avoid SQL injection
+        session.createSQLQuery("DELETE FROM vehicle_report_item WHERE vehicle_id = "  + id)   // TODO: avoid SQL injection
 //                .setParameter("xxx", id)
                 .executeUpdate();
 
