7d7cddbdf85bcf9e05935a6ed83f9f85b0c81c9d
jevgeni <zeka.rum@gmail.com>
2017-01-08 15:43:23 +0200

    friendly errors

--- skynda_backend/src/java/me/skynda/common/dto/SimpleResponseDto.java
+++ skynda_backend/src/java/me/skynda/common/dto/SimpleResponseDto.java
@@ -3,7 +3,9 @@ package me.skynda.common.dto;
 import lombok.Data;
 import org.springframework.validation.ObjectError;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by zekar on 1/8/2017.
@@ -14,12 +16,25 @@ public class SimpleResponseDto {
 
     private boolean success;
     private List<ObjectError> errors;
+    private Map<String, String> friendlyErrors;
 
     public static class Factory {
         public static SimpleResponseDto fail(List<ObjectError> errors) {
             SimpleResponseDto dto = new SimpleResponseDto();
             dto.setSuccess(false);
             dto.setErrors(errors);
+
+            try {
+                Map<String, String> friendlyErrors = new LinkedHashMap<>();
+                errors.forEach(error -> {
+                    if (!friendlyErrors.containsKey(error.getCode()))
+                        friendlyErrors.put(error.getCode(), error.getDefaultMessage());
+                });
+                dto.setFriendlyErrors(friendlyErrors);
+            } catch (Exception e) {
+
+            }
+
             return dto;
         }
 
