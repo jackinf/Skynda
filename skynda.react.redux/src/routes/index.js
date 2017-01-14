5b47d97de2918d4d5a6ecb054f1f1ee6a4c33f5f
jevgenir <jevgeni.rumjantsev@finestmedia.ee>
2017-01-14 19:36:53 +0200

    ememe

--- skynda.react.redux/src/routes/index.js
+++ skynda.react.redux/src/routes/index.js
@@ -27,14 +27,14 @@ export const createRoutes = (store) => {
   injectReducer(store, {key: "toastr", reducer: require("react-redux-toastr").reducer});
   injectReducer(store, {key: "auth", reducer: require("./Auth/modules/auth.module").default});
   syncTranslationWithStore(store);
-  store.dispatch(loadTranslations({
-    et: {},
-    en: {}
-  }));
   // store.dispatch(loadTranslations({
-  //   et: require("./../store/translations/et.json"),
-  //   en: require("./../store/translations/en.json")
+  //   et: {},
+  //   en: {}
   // }));
+  store.dispatch(loadTranslations({
+    et: require("./../store/translations/et.json"),
+    en: require("./../store/translations/en.json")
+  }));
   store.dispatch(setLocale('et'));
 
   injectReducer(store, {key: "form", reducer: formReducer});
