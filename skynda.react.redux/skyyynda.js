875a2b58e03cc9dfd44eb2860f09d1f60b97bd42
jevgenir <jevgeni.rumjantsev@finestmedia.ee>
2017-02-04 15:34:59 +0200

    cors

--- skynda.react.redux/skyyynda.js
+++ skynda.react.redux/skyyynda.js
@@ -1,70 +1,33 @@
-// var express = require('express');
-// var app = express();
-// app.use('/', express.static(__dirname + '/dist')); // ? adjust
-// app.listen(3000, function() { console.log('listening'); });
-
 const express = require('express');
-//const exec = require('child_process').exec;
-require('shelljs/global');
 const path = require('path');
+const cors = require('cors');
 const port = 80; //process.env.PORT || 3000;
 const app = express();
 
-var pmx = require('pmx').init({
-  http          : true, // HTTP routes logging (default: true)
-  ignore_routes : [/socket\.io/, /notFound/], // Ignore http routes with this pattern (Default: [])
-  errors        : true, // Exceptions loggin (default: true)
-  custom_probes : true, // Auto expose JS Loop Latency and HTTP req/s as custom metrics
-  network       : true, // Network monitoring at the application level
-  ports         : true  // Shows which ports your app is listening on (default: false)
-});
-
-pmx.action('hello:world', function(reply) {
-  reply({success : true});
-});
-
-
-function run_cmd(cmd, args, callBack ) {
-    var spawn = require('child_process').spawn;
-    var child = spawn(cmd, args);
-    var resp = "";
-
-    child.stdout.on('data', function (buffer) { resp += buffer.toString() });
-    child.stdout.on('end', function() { callBack (resp) });
-} // ()
-
-pmx.action('deploy:prod', function(reply) {
-  run_cmd( "npm run deploy:prod", function(text) { console.log (text) });
-  repply({success: true});
-/*
-  if (exec('git fetch -p').code !== 0) {
-  	reply({success: false}});
-  	return;
-  } 
-  if (exec('git pull').code !== 0) {
-  	reply({success: false}});
-  	return;
-  } 
-  if (exec('npm i').code !== 0) {
-  	reply({success: false}});
-  	return;
-  } 
-  if (exec('npm run deploy:prod').code !== 0) {
-  	reply({success: false}});
-  	return;
-  } 
-  reply({success : true});
-  */
-/*
-  exec('deploy-skynda.sh', (error, stdout, stderr) => {
-    if (error) {
-      reply({success: false, error: error, v: '1'});
-    } else {
-      reply({success : true});
+const whitelist = ['http://triven.eu'];
+const corsOptions = {
+  origin: function(origin, callback){
+    if (!origin) {
+      callback(null, true);
+      return;
     }
-  });
-*/
-});
+    const originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+    callback(origin && originIsWhitelisted ? null : 'Bad Request', originIsWhitelisted);
+  }
+};
+
+//enable cors
+app.use(cors(corsOptions));
+
+// Alternative way
+// //enable cors
+// app.use(cors({
+//   'allowedHeaders': ['sessionId', 'Content-Type'],
+//   'exposedHeaders': ['sessionId'],
+//   'origin': '*',
+//   'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   'preflightContinue': false
+// }));
 
 // serve static assets normally
 app.use(express.static(__dirname + '/dist'));
