b9ea99ff3df94f4cb083868be18ce7c4126245c9
jevgenir <jevgeni.rumjantsev@finestmedia.ee>
2017-02-11 15:40:14 +0200

    build fix

--- skynda_backend/src/java/me/skynda/classification/dao/ClassificationDao.java
+++ skynda_backend/src/java/me/skynda/classification/dao/ClassificationDao.java
@@ -1,10 +1,11 @@
 package me.skynda.classification.dao;
 
-import me.skynda.classification.entities.Classification;
 import me.skynda.classification.enums.ClassificationTypeEnum;
 import me.skynda.common.db.BaseEntityDao;
+import me.skynda.common.entities.Classification;
+import me.skynda.common.entities.VehicleModel;
+import me.skynda.common.helper.JsonHelper;
 import me.skynda.common.interfaces.daos.IClassificationDao;
-import me.skynda.vehicle.entities.VehicleModel;
 import org.hibernate.Criteria;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
