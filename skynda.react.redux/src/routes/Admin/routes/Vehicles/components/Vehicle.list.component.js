c3a6f70f917c414576d64873746414fa41adb4d3
jevgenir <jevgeni.rumjantsev@finestmedia.ee>
2017-02-11 16:01:27 +0200

    refactoring - vehicle

--- skynda.react.redux/src/routes/Admin/routes/Vehicles/components/Vehicle.list.component.js
+++ skynda.react.redux/src/routes/Admin/routes/Vehicles/components/Vehicle.list.component.js
@@ -25,48 +25,49 @@ export default class VehicleList extends React.Component {
     let rows = !this.props.data.isFetching ? this.props.data.items : [];
     const loading = this.props.data.isFetching ? "Fetching" : "Vehicles";
 
-    return (<div className="container">
-      <h3>{loading}</h3>
+    return (<span>
+      {this.props.children ?  this.props.children : (<div className="container">
+          <h3>{loading}</h3>
 
-      <RaisedButton secondary={true} label="Add" onClick={e => browserHistory.push(`/admin/vehicle/new`)}/>
+          <RaisedButton secondary={true} label="Add" onClick={e => browserHistory.push(`/admin/vehicle/new`)}/>
 
-      <Table rowHeight={50} rowsCount={rows.length} width={1000} maxHeight={500} headerHeight={50}>
-        <Column
-          header={<Cell>#</Cell>}
-          cell={({rowIndex, ...props}) => (
-            <Cell {...props}>
-              {rowIndex + 1}.
-            </Cell>
-          )}
-          width={50}
-        />
-        <Column
-          header={<Cell>Id</Cell>}
-          cell={({rowIndex, ...props}) => (<Cell {...props}>
-            <a href={`/details/${rows[rowIndex].id}`} target="_blank">{rows[rowIndex].id}</a>
-          </Cell>)}
-          width={200}
-        />
-        <Column
-          header={<Cell>Name</Cell>}
-          cell={({rowIndex, ...props}) => {
-            return (<Cell {...props}>{rows[rowIndex].model ? rows[rowIndex].model.modelCode : ""}</Cell>);
-          }}
-          width={200}
-        />
-        <Column
-          header={<Cell>Actions</Cell>}
-          cell={({rowIndex, ...props}) => (
-            <Cell {...props}>
-              {/*<Link to={`/admin/car/${rows[rowIndex].id}`}>Show</Link>*/}
-              <RaisedButton label="Show" onClick={e => browserHistory.push(`/admin/vehicle/${rows[rowIndex].id}`)}/>
-              <RaisedButton secondary={true} label="Delete" onClick={e => this.props.deleteItem(rows[rowIndex].id)}/>
-            </Cell>
-          )}
-          width={200}
-        />
-      </Table>
-
-    </div>)
+          <Table rowHeight={50} rowsCount={rows.length} width={1000} maxHeight={500} headerHeight={50}>
+            <Column
+              header={<Cell>#</Cell>}
+              cell={({rowIndex, ...props}) => (
+                <Cell {...props}>
+                  {rowIndex + 1}.
+                </Cell>
+              )}
+              width={50}
+            />
+            <Column
+              header={<Cell>Id</Cell>}
+              cell={({rowIndex, ...props}) => (<Cell {...props}>
+                <a href={`/details/${rows[rowIndex].id}`} target="_blank">{rows[rowIndex].id}</a>
+              </Cell>)}
+              width={200}
+            />
+            <Column
+              header={<Cell>Name</Cell>}
+              cell={({rowIndex, ...props}) => {
+                return (<Cell {...props}>{rows[rowIndex].model ? rows[rowIndex].model.modelCode : ""}</Cell>);
+              }}
+              width={200}
+            />
+            <Column
+              header={<Cell>Actions</Cell>}
+              cell={({rowIndex, ...props}) => (
+                <Cell {...props}>
+                  {/*<Link to={`/admin/car/${rows[rowIndex].id}`}>Show</Link>*/}
+                  <RaisedButton label="Show" onClick={e => browserHistory.push(`/admin/vehicle/${rows[rowIndex].id}`)}/>
+                  <RaisedButton secondary={true} label="Delete" onClick={e => this.props.deleteItem(rows[rowIndex].id)}/>
+                </Cell>
+              )}
+              width={200}
+            />
+          </Table>
+      </div>)}
+    </span>)
   }
 }
--- skynda.react.redux/src/routes/Admin/routes/Vehicles/index.js
+++ skynda.react.redux/src/routes/Admin/routes/Vehicles/index.js
@@ -1,50 +1,20 @@
 import {injectReducer} from '../../../../store/reducers';
 import {ROUTE_PARAMS, FORM_MODE, REDUCER_KEYS} from "./constants/Vehicle.constant";
-import {REDUCER_KEYS as REPORT_REDUCER_KEYS} from "../VehicleReports/constants/VehicleReport.constant";
-import {REDUCER_KEYS as REVIEW_REDUCER_KEYS} from "../VehicleReviews/constants/VehicleReview.constant";
-import {setVehicleReportData, setFormMode as setFormModeReport} from "../VehicleReports/reducers";
-import {setVehicleReviewData, setFormMode as setFormModeReview} from "../VehicleReviews/reducers";
-import {setVehicleReportsList} from "./reducers/Vehicle.Reports.reducer";
-import {setVehicleReviewsList} from "./reducers/Vehicle.Reviews.reducer";
 import NProgress from "react-nprogress";
-
+import VehicleRoute from "./routes/Vehicle";
 
 export default (store) => ({
-  path: `vehicle(/:${ROUTE_PARAMS.VEHICLE_ID})`,
+  path: `vehicle`,
   getComponent(nextState, cb) {
     NProgress.start();
     require.ensure([], (require) => {
-      const id = nextState.params[ROUTE_PARAMS.VEHICLE_ID];
-      const formMode = id === ROUTE_PARAMS.values.NEW
-        ? FORM_MODE.ADDING : !isNaN(parseInt(id))
-        ? FORM_MODE.UPDATING : FORM_MODE.NONE;
-
-      if (formMode == FORM_MODE.ADDING || formMode == FORM_MODE.UPDATING) {
-        injectReducer(store, {key: REDUCER_KEYS.VEHICLE_DATA, reducer: require("./reducers/Vehicle.reducer.js").default});
-        injectReducer(store, {key: "classificators", reducer: require("./../Classifiers/Classifiers.module").default});
-        injectReducer(store, {key: "formInfo", reducer: require("./../VehicleModels/reducers/VehicleModel.reducer.js").default});
-
-        injectReducer(store, {key: REDUCER_KEYS.VEHICLE_MODELS_DATA, reducer: require("./../VehicleModels/reducers/VehicleModels.reducer.js").default});
-
-        //FOR VEHICLE REPORTS
-        injectReducer(store, {key: REDUCER_KEYS.VEHICLE_REPORTS_DATA_LIST, reducer: setVehicleReportsList});
-        injectReducer(store, {key: REPORT_REDUCER_KEYS.VEHICLE_REPORT_DATA, reducer: setVehicleReportData});
-        injectReducer(store, {key: REPORT_REDUCER_KEYS.FORM_MODE_VEHICLE_REPORT, reducer: setFormModeReport});
-
-        //FOR VEHICLE REVIEWS
-        injectReducer(store, {key: REDUCER_KEYS.VEHICLE_REVIEWS_DATA_LIST, reducer: setVehicleReviewsList});
-        injectReducer(store, {key: REVIEW_REDUCER_KEYS.VEHICLE_REVIEW_DATA, reducer: setVehicleReviewData});
-        injectReducer(store, {key: REVIEW_REDUCER_KEYS.FORM_MODE_VEHICLE_REVIEW, reducer: setFormModeReview});
-
-        cb(null, require("./containers/Vehicle.container.js").default);
-        NProgress.done();
-      }
-      else {
-        injectReducer(store, {key: REDUCER_KEYS.VEHICLES_DATA, reducer: require("./reducers/Vehicles.list.reducer.js").default});
-        cb(null, require("./containers/Vehicle.list.container.js").default);
-        NProgress.done();
-      }
+      injectReducer(store, {key: REDUCER_KEYS.VEHICLES_DATA, reducer: require("./reducers/Vehicles.list.reducer.js").default});
+      cb(null, require("./containers/Vehicle.list.container.js").default);
+      NProgress.done();
     })
-  }
+  },
+  childRoutes: [
+    VehicleRoute(store)
+  ]
 
 })
