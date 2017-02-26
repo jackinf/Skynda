c230594ba23b0d6426afee14288d7af570a775a6
jevgeni <zeka.rum@gmail.com>
2017-02-26 17:33:16 +0200

    deployment configuration

--- skynda_dotnet_backend/Triven.Api/Startup.cs
+++ skynda_dotnet_backend/Triven.Api/Startup.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Http;
+﻿using System.Net.Http.Headers;
+using System.Web.Http;
 using Microsoft.Owin;
 using Newtonsoft.Json.Serialization;
 using Owin;
@@ -20,6 +21,10 @@ namespace Triven.API
             HttpConfiguration config = GlobalConfiguration.Configuration;
             config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
             config.Formatters.JsonFormatter.UseDataContractJsonSerializer = false;
+
+            // This will tell to return everything in JSON. This is needed to avoid this error when calling api actions: https://gyazo.com/19fad821add4057a3b42b57a1921d71c
+            // Source: http://stackoverflow.com/questions/9847564/how-do-i-get-asp-net-web-api-to-return-json-instead-of-xml-using-chrome
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
         }
     }
 }
