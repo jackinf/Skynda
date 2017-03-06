b62576575176b662522d2347880f127fca1e5730
Ardi Aver <reva.ardi@gmail.com>
2017-03-06 19:23:08 +0200

    kaka

--- skynda.react.redux/src/webServices/VehicleServices.js
+++ skynda.react.redux/src/webServices/VehicleServices.js
@@ -6,35 +6,30 @@ export const VehicleService = {
   deleteItem: function (id) {
     return handle(fetch(`${remoteConfig.remote}/api/vehicle/${id}`, {
       method: "DELETE",
-      mode: 'no-cors',
       headers: getDefaultHeaders()
     }));
   },
   getList: function () {
     return handle(fetch(`${remoteConfig.remote}/api/vehicles`, {
       method: "GET",
-      mode: 'no-cors',
       headers: getDefaultHeaders()
     }))
   },
   fetchItem: function (id) {
     return handle(fetch(`${remoteConfig.remote}/api/vehicle/${id}`, {
       method: "GET",
-      mode: 'no-cors',
       headers: getDefaultHeaders()
     }));
   },
   getDataAsync: function (id) {
     return handle(fetch(`${remoteConfig.remote}/api/vehicle/${id}/detailed`, {
       method: "GET",
-      mode: 'no-cors',
       headers: getDefaultHeaders()
     }));
   },
   search: function (searchValues) {
     return handle(fetch(`${remoteConfig.remote}/api/vehicle/search`, {
       method: "POST",
-      mode: 'no-cors',
       headers: getDefaultHeaders(),
       body: JSON.stringify(searchValues)
     }));
@@ -42,7 +37,6 @@ export const VehicleService = {
   createItem: function (data) {
     return handle(fetch(`${remoteConfig.remote}/api/vehicle`, {
       method: "POST",
-      mode: 'no-cors',
       headers: getDefaultHeaders(),
       body: JSON.stringify(data)
     }));
@@ -50,7 +44,6 @@ export const VehicleService = {
   updateItem: function (data) {
     return handle(fetch(`${remoteConfig.remote}/api/vehicle/${data.id}`, {
       method: "PUT",
-      mode: 'no-cors',
       headers: getDefaultHeaders(),
       body: JSON.stringify(data)
     }));
