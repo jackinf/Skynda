45011bd5de22109afc22ca7fc1e82d771b831426
jevgenir <jevgeni.rumjantsev@finestmedia.ee>
2017-03-11 15:31:07 +0200

    get single fix

---
+++ skynda_dotnet_backend/Triven.Data.EntityFramework/Exceptions/ContextIsNullException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Triven.Data.EntityFramework.Exceptions
+{
+    public class ContextIsNullException : Exception
+    {
+        public ContextIsNullException() : base("Context is required to proceed")
+        {
+        }
+    }
+}
