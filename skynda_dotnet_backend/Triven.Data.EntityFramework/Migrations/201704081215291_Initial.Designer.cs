51fecb4b5ae01f2dac3678838c50d196f40f8b7a
jevgenir <jevgeni.rumjantsev@finestmedia.ee>
2017-04-08 15:18:02 +0300

    Migrations reset

--- skynda_dotnet_backend/Triven.Data.EntityFramework/Migrations/201704081215291_Initial.Designer.cs
+++ skynda_dotnet_backend/Triven.Data.EntityFramework/Migrations/201704081215291_Initial.Designer.cs
@@ -7,13 +7,13 @@ namespace Triven.Data.EntityFramework.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class VehicleMigrationReq : IMigrationMetadata
+    public sealed partial class Initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(VehicleMigrationReq));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201703041042226_VehicleMigrationReq"; }
+            get { return "201704081215291_Initial"; }
         }
         
         string IMigrationMetadata.Source
