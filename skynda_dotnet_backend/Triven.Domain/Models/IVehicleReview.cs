27cf91f215a2875eabd493b6283ef634cfa5dec3
Ardi Aver <reva.ardi@gmail.com>
2017-04-08 13:40:49 +0300

    vehicle reviews migration

--- skynda_dotnet_backend/Triven.Domain/Models/IVehicleReview.cs
+++ skynda_dotnet_backend/Triven.Domain/Models/IVehicleReview.cs
@@ -4,8 +4,10 @@ namespace Triven.Domain.Models
 {
     public interface IVehicleReview : IAuditableBaseModel
     {
-        int LogoId { get; set; }
-        int VideoId { get; set; }
+        //int LogoId { get; set; }
+        //int VideoId { get; set; }
+        string VideoUrl { get; set; }
+        string LogoUrl { get; set; }
         int Rating { get; set; }
         string Text { get; set; }
     }
