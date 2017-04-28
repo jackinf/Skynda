f2b20d85e43d0815e065964e782646777da6da1a
jevgenir <jevgeni.rumjantsev@finestmedia.ee>
2017-04-28 12:24:47 +0300

    telemetry for API

--- skynda_dotnet_backend/Triven.Api/App_Start/FilterConfig.cs
+++ skynda_dotnet_backend/Triven.Api/App_Start/FilterConfig.cs
@@ -6,7 +6,7 @@ namespace Triven.API
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
         }
     }
 }
